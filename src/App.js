734fc10f91a601037189c04ea52e1ec3bba431e2
kly001 <karenly1961@gmail.com>
2019-08-17 12:13:10 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
-import AppRouter from "./components/AppRouter.js";
+import WelcomePage from "./components/WelcomePage"
+// import AppRouter from "./components/AppRouter.js";
 
 export default function App() {
   return (
     <main>
       <Header />
-      <TabNav />
-      <AppRouter />
+      <WelcomePage />
+      {/* <TabNav /> */}
+      {/* <AppRouter /> */}
     </main>
   );
 }
