479baf74cddb806c5551602810624042fdd71393
kly001 <karenly1961@gmail.com>
2019-08-17 12:50:42 -0400

    Edited CharacterList & CharacterCard

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 import WelcomePage from "./components/WelcomePage"
+import CharacterList from "./components/CharacterList"
 // import AppRouter from "./components/AppRouter.js";
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
     <main>
       <Header />
       <WelcomePage />
+      <CharacterList />
       {/* <TabNav /> */}
       {/* <AppRouter /> */}
     </main>
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return <span>
+    <div className ="character-info">
+      <img src={props.image} alt="character pic"/>
+      <p>{props.name}</p>
+      <p>{props.status}</p>
+      <p>{props.species}</p>
+    </div>
+  </span>;
 }
