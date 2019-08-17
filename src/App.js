306e423bb18d623d896dd3e316889fa0d355873f
kly001 <karenly1961@gmail.com>
2019-08-17 14:36:58 -0400

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from "react";
 import {Route} from "react-router-dom";
-import{TabNav, Header, WelcomePage, CharacterList, LocationsList, EpisodesList} from "./components"
-// import TabNav from "./components/TabNav.js";
-// import Header from "./components/Header.js";
-// import WelcomePage from "./components/WelcomePage"
-// import CharacterList from "./components/CharacterList"
-// import LocationsList from "./components/LocationsList"
-// import EpisodesList from "./components/EpisodesList"
+import TabNav from "./components/TabNav.js";
+import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage"
+import CharacterList from "./components/CharacterList"
+import LocationsList from "./components/LocationsList"
+import EpisodesList from "./components/EpisodesList"
 // import AppRouter from "./components/AppRouter.js";
 
 export default function App() {
   return (
     <main>
       <Header />
-      {/* <WelcomePage />
+      <WelcomePage />
       <CharacterList />
       <LocationsList />
       <EpisodesList />
-       <TabNav /> 
-      <AppRouter /> */}
+       {/* <TabNav /> 
+      <AppRouter />  */}
+
+  
     </main>
   );
 }
